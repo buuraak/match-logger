094ae48b89ca41c91d8f1f0ec14fd522a8c4a380
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-07-06 21:56:39 +0200

    Add: interactivity to action buttons

--- src/app/match/components/ActionLogger.js
+++ src/app/match/components/ActionLogger.js
@@ -1,13 +1,74 @@
 export default function ActionLogger() {
+    const goalClick = (e) => {
+        let team;
+
+        if (e.target.dataset.goal === "home") {
+            team = "home"
+        } else {
+            team = "away"
+        }
+
+        fetch('/api/goalLogger', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "team": team })
+        })
+    }
+    const cardClick = (e) => {
+        let team;
+        let card;
+
+        if (e.target.dataset.team === "home") {
+            team = "home"
+        } else {
+            team = "away"
+        }
+
+        if (e.target.dataset.card === "yellow") {
+            card = "Gele kaart"
+        } else if (e.target.dataset.card === "red") {
+            card = "Rode kaart"
+        } else {
+            card = "Groene kaart"
+        }
+
+        fetch('/api/cardLogger', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "team": team, "card": card })
+        })
+    }
+    const penaltyClick = (e) => {
+        let team;
+
+        if (e.target.dataset.team === "home") {
+            team = "home"
+        } else {
+            team = "away"
+        }
+
+        fetch('/api/penaltyLogger', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "team": team })
+        })
+
+    }
     return (
         <div className="h-[700px] w-auto grid grid-cols-2 bg-white rounded-xl shadow-lg">
             <div className="text-black h-full border-r">
                 <div className="h-1/2">
-                    <p className="text-2xl font-extrabold border-b pl-8 py-5 rounded-tl-xl hover:bg-primary hover:text-white duration-100">Doelpunt</p>
-                    <p className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
-                    <p className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
-                    <p className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
-                    <p className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
+                    <p onClick={goalClick} data-goal="home" className="text-2xl font-extrabold border-b pl-8 py-5 rounded-tl-xl hover:bg-primary hover:text-white duration-100">Doelpunt</p>
+                    <p onClick={cardClick} data-card="yellow" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
+                    <p onClick={cardClick} data-card="red" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
+                    <p onClick={cardClick} data-card="green" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
+                    <p onClick={penaltyClick} data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
                 </div>
                 <div className="h-1/2 flex justify-center items-end pb-9">
                     <p className="font-extrabold text-2xl opacity-20 text-center">THUIS</p>
@@ -15,11 +76,11 @@ export default function ActionLogger() {
             </div>
             <div className="text-black text-right h-full">
                 <div className="h-1/2">
-                    <p className="text-2xl font-extrabold rounded-tr-xl border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Doelpunt</p>
-                    <p className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
-                    <p className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
-                    <p className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
-                    <p className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
+                    <p onClick={goalClick} data-goal="away" className="text-2xl font-extrabold rounded-tr-xl border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Doelpunt</p>
+                    <p onClick={cardClick} data-card="yellow" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
+                    <p onClick={cardClick} data-card="red" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
+                    <p onClick={cardClick} data-card="green" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
+                    <p onClick={penaltyClick} data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
                 </div>
                 <div className="h-1/2 flex justify-center items-end pb-9">
                     <p className="font-extrabold text-2xl opacity-20 text-center">UIT</p>
