ede07076c1ad29758d5c7af9e4c14bfedf148341
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-07-06 21:51:01 +0200

    Update: Add timestamps to Match model

--- prisma/data.js
+++ prisma/data.js
@@ -2,6 +2,8 @@ const matchData = [
     {
         goalsHome: 0,
         goalsAway: 0,
+        startingTimestamp: null,
+        lastTimestamp: null,
     },
     // Add more match data if needed
 ];
