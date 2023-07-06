b6886ed2a9911dadcf75891956fcb4b3ec56d6a4
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-07-06 21:54:36 +0200

    Feature: Fetch match & match actions from the database

--- src/app/match/page.js
+++ src/app/match/page.js
@@ -1,11 +1,40 @@
+"use client"
 import ActionLogger from "./components/ActionLogger"
 import MatchDetails from "./components/MatchDetails"
+import { useEffect, useState } from "react"
 
 export default function Match() {
+    const [match, setMatch] = useState({
+        goalsHome: 0,
+        goalsAway: 0,
+    });
+    const [actions, setActions] = useState([{
+        action: '',
+        team: ''
+    }]);
+
+    useEffect(() => {
+        const fetchMatch = async () => {
+            const response = await fetch('/api/matches');
+            const data = await response.json();
+            setMatch(data.data);
+        }
+        fetchMatch();
+    }, []);
+
+    useEffect(() => {
+        const fetchActions = async () => {
+            const response = await fetch('/api/actions');
+            const data = await response.json();
+            setActions(data.data);
+        }
+        fetchActions();
+    }, []);
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2">
             <ActionLogger />
-            <MatchDetails />
+            <MatchDetails actions={actions} match={match} />
         </div>
     )
 }
