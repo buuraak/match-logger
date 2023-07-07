58e81b6f8f8228a141cc00bb21965a442c4e37d9
Burak Baykara <32229536+buuraak@users.noreply.github.com>
2023-07-07 11:13:09 +0200

    Update: combine logger api routing
    Instead of creating separate api routing for all the actions. I create one api route and use switch to execute a certain action.

---
+++ src/app/api/ActionLogger/route.js
@@ -0,0 +1,103 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req) {
+    const request = await req.json();
+    const team = request.team;
+    const actionType = request.actionType;
+    try {
+        switch (actionType) {
+            case "goal":
+                if (team == "home") {
+                    await prisma.match.update({
+                        where: {
+                            id: 1
+                        },
+                        data: {
+                            goalsHome: {
+                                increment: 1
+                            }
+                        }
+                    });
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: "Doelpunt",
+                            timestamp: new Date(),
+                            team: "Thuis"
+                        }
+                    });
+                } else {
+                    await prisma.match.update({
+                        where: {
+                            id: 1
+                        },
+                        data: {
+                            goalsAway: {
+                                increment: 1
+                            }
+                        }
+                    });
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: "Doelpunt",
+                            timestamp: new Date(),
+                            team: "Uit"
+                        }
+                    });
+                }
+                break;
+            case "card":
+                const card = request.card;
+                if (team == "home") {
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: card,
+                            timestamp: new Date(),
+                            team: "Thuis"
+                        }
+                    });
+                } else {
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: card,
+                            timestamp: new Date(),
+                            team: "Uit"
+                        }
+                    });
+                }
+                break;
+            case "penalty":
+                if (team == "home") {
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: "Penalty",
+                            timestamp: new Date(),
+                            team: "Thuis"
+                        }
+                    });
+                } else {
+                    await prisma.action.create({
+                        data: {
+                            matchId: 1,
+                            action: "Penalty",
+                            timestamp: new Date(),
+                            team: "Uit"
+                        }
+                    });
+                }
+                break;
+        }
+    } catch (error) {
+        console.log(`Error at: ${actionType}, error: ${error}`);
+        NextResponse.json({ "message": `Error at: ${actionType}, error: ${error}`, "status": 500 })
+    }
+
+    NextResponse.json({ "message": `Successfuly logged ${actionType}`, "status": 200 })
+}
--- src/app/match/components/ActionLogger.js
+++ src/app/match/components/ActionLogger.js
@@ -1,74 +1,66 @@
 export default function ActionLogger() {
-    const goalClick = (e) => {
-        let team;
 
-        if (e.target.dataset.goal === "home") {
-            team = "home"
-        } else {
-            team = "away"
-        }
-
-        fetch('/api/goalLogger', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "team": team })
-        })
-    }
-    const cardClick = (e) => {
+    const logHandler = async (e) => {
         let team;
         let card;
+        const actionType = e.target.dataset.type;
 
         if (e.target.dataset.team === "home") {
             team = "home"
         } else {
             team = "away"
         }
+        try {
+            switch (actionType) {
+                case "goal":
+                    await fetch('/api/ActionLogger', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ "team": team, "actionType": actionType })
+                    })
+                    break;
+                case "card":
+                    if (e.target.dataset.card === "yellow") {
+                        card = "Gele kaart"
+                    } else if (e.target.dataset.card === "red") {
+                        card = "Rode kaart"
+                    } else {
+                        card = "Groene kaart"
+                    }
 
-        if (e.target.dataset.card === "yellow") {
-            card = "Gele kaart"
-        } else if (e.target.dataset.card === "red") {
-            card = "Rode kaart"
-        } else {
-            card = "Groene kaart"
+                    await fetch('/api/ActionLogger', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ "team": team, "card": card, "actionType": actionType })
+                    })
+                    break;
+                case "penalty":
+                    await fetch('/api/ActionLogger', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ "team": team, "actionType": actionType })
+                    })
+                    break;
+            }
+        } catch (error) {
+            console.log(`Error at : ${actionType} ${error}`)
         }
-
-        fetch('/api/cardLogger', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "team": team, "card": card })
-        })
-    }
-    const penaltyClick = (e) => {
-        let team;
-
-        if (e.target.dataset.team === "home") {
-            team = "home"
-        } else {
-            team = "away"
-        }
-
-        fetch('/api/penaltyLogger', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "team": team })
-        })
-
     }
     return (
         <div className="h-[700px] w-auto grid grid-cols-2 bg-white rounded-xl shadow-lg">
             <div className="text-black h-full border-r">
                 <div className="h-1/2">
-                    <p onClick={goalClick} data-goal="home" className="text-2xl font-extrabold border-b pl-8 py-5 rounded-tl-xl hover:bg-primary hover:text-white duration-100">Doelpunt</p>
-                    <p onClick={cardClick} data-card="yellow" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
-                    <p onClick={cardClick} data-card="red" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
-                    <p onClick={cardClick} data-card="green" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
-                    <p onClick={penaltyClick} data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
+                    <p onClick={logHandler} data-type="goal" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 rounded-tl-xl hover:bg-primary hover:text-white duration-100">Doelpunt</p>
+                    <p onClick={logHandler} data-type="card" data-card="yellow" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
+                    <p onClick={logHandler} data-type="card" data-card="red" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
+                    <p onClick={logHandler} data-type="card" data-card="green" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
+                    <p onClick={logHandler} data-type="penalty" data-team="home" className="text-2xl font-extrabold border-b pl-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
                 </div>
                 <div className="h-1/2 flex justify-center items-end pb-9">
                     <p className="font-extrabold text-2xl opacity-20 text-center">THUIS</p>
@@ -76,11 +68,11 @@ export default function ActionLogger() {
             </div>
             <div className="text-black text-right h-full">
                 <div className="h-1/2">
-                    <p onClick={goalClick} data-goal="away" className="text-2xl font-extrabold rounded-tr-xl border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Doelpunt</p>
-                    <p onClick={cardClick} data-card="yellow" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
-                    <p onClick={cardClick} data-card="red" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
-                    <p onClick={cardClick} data-card="green" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
-                    <p onClick={penaltyClick} data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
+                    <p onClick={logHandler} data-type="goal" data-team="away" className="text-2xl font-extrabold rounded-tr-xl border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Doelpunt</p>
+                    <p onClick={logHandler} data-type="card" data-card="yellow" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Gele kaart</p>
+                    <p onClick={logHandler} data-type="card" data-card="red" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Rode kaart</p>
+                    <p onClick={logHandler} data-type="card" data-card="green" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Groene kaart</p>
+                    <p onClick={logHandler} data-type="penalty" data-team="away" className="text-2xl font-extrabold border-b pr-8 py-5 hover:bg-primary hover:text-white duration-100">Penalty</p>
                 </div>
                 <div className="h-1/2 flex justify-center items-end pb-9">
                     <p className="font-extrabold text-2xl opacity-20 text-center">UIT</p>
